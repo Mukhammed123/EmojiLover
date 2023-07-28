7f883881d941ce31f3166888966985ee647aceed
mukhammed musa <muhammedmusaaa@gmail.com>
2023-07-28 20:26:38 +0600

    first commit

---
+++ EmohiLover/EmohiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmohiLoverApp.swift
+//  EmohiLover
+//
+//  Created by Mukhammed Musa on 28/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EmohiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
